83310b663cdca8d6bfbeb8b01506b1ec5fe2e77b
Nikolaisedoi <82058276+Nikolaisedoi@users.noreply.github.com>
2021-10-12 20:40:59 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 $(document).ready(function(){
-  $('.single-item').slick({
-		arrows: true,
+  $('.gallery-slider').slick({
+    arrows: true,
 		infinite: true,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-    prevArrow:'<img class="arrows prevarrow" src="img/prevarrow.svg" alt="prevarrow">',
-    nextArrow:'<img src="img/nextarrow.svg" alt="nextarrow" class="arrows nextarrow">'
+		prevArrow: '<button type = "button" class = "slick-btn slick-prev"><img src="img/prevarrow.svg" alt="prevarrow" /></ button>',
+    nextArrow: '<button type = "button" class = "slick-btn slick-next"><img src="img/nextarrow.svg" alt="nextarrow" /></ button>'
   });
+
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+	});
+
+	$('.popup-link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+
+	$('.menu-burger').on('click', function(){
+		$('.menu-list').toggleClass('menu-list_active');
+	});
 });
