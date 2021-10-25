f279217d9ce85f8de1b458c42d3e98b735b3ee7a
Nikolaisedoi <82058276+Nikolaisedoi@users.noreply.github.com>
2021-10-25 22:58:48 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+//slider
 $(document).ready(function(){
   $('.gallery-slider').slick({
     arrows: true,
 		infinite: true,
 		prevArrow: '<button type = "button" class = "slick-btn slick-prev"><img src="img/prevarrow.svg" alt="prevarrow" /></ button>',
-    nextArrow: '<button type = "button" class = "slick-btn slick-next"><img src="img/nextarrow.svg" alt="nextarrow" /></ button>'
+    nextArrow: '<button type = "button" class = "slick-btn slick-next"><img src="img/nextarrow.svg" alt="nextarrow" /></ button>',
   });
 
+
+
+//gallery
 	$('.popup-gallery').magnificPopup({
 		delegate: 'a',
 		type: 'image',
@@ -18,6 +22,9 @@ $(document).ready(function(){
 		},
 	});
 
+
+
+//popup-video
 	$('.popup-link').magnificPopup({
 		disableOn: 700,
 		type: 'iframe',
@@ -27,7 +34,13 @@ $(document).ready(function(){
 		fixedContentPos: false
 	});
 
+
+// burger-menu
 	$('.menu-burger').on('click', function(){
 		$('.menu-list').toggleClass('menu-list_active');
 	});
 });
+
+
+
+ 	 
